a782f0122b9c5450a60eeec8f5e0542c56f4fa0e
Gabriel Urano <gabrielurano30@alu.ufc.br>
2023-05-03 20:00:07 -0300

    fix: correções acomodações

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,27 +1,31 @@
-import { Container,Information,Title, Phrase } from "./style";
+import { Container, Information, Title, Phrase, Image, InfoContainer, ButtonContainer } from "./style";
 
 import { getWindowSize } from '../../utils/getWindowSize';
 import { Button } from "../Button";
-
-import foto from '../../assets/Foto.png'
 
 const Description = () =>{
     const windowSize = getWindowSize();
     return(
         <Container device={windowSize}>
-            <Information  device={windowSize}>
-                <Title device={windowSize}>
-                    Confira nossas acomodações
-                </Title>
-                <Phrase>
-                    Aproveita as várias atrações turísticas, como a Igreja do Céu, a Igreja Matriz de Nossa Senhora da Assunção, o Teatro Pedro II, a Cachoeira do Itarumã, a Cachoeira da Pirapora, a Cachoeira do Engenho Velho e a Pedra do Machado.
-                    <br/>
-                    <br/>
-                    As igrejas possuem arquiteturas impressionantes, o teatro é um espaço cultural movimentado, enquanto as cachoeiras e a pedra atraem visitantes em busca de aventuras e vistas panorâmicas.
-                </Phrase>
-                <Button type="secondary" text="Reservar minha suíte" onClick={()=>console.log("Clicou")} size={windowSize === 'mobile'? 'xlarge':'large'} />
+            <Information device={windowSize}>
+                <InfoContainer device={windowSize}>
+                    <Title device={windowSize}>
+                        Confira nossas acomodações
+                    </Title>
+                    <Phrase>
+                        Aproveita as várias atrações turísticas, como a Igreja do Céu, a Igreja Matriz de Nossa Senhora da Assunção, o Teatro Pedro II, a Cachoeira do Itarumã, a Cachoeira da Pirapora, a Cachoeira do Engenho Velho e a Pedra do Machado.
+                        <br/>
+                        <br/>
+                        As igrejas possuem arquiteturas impressionantes, o teatro é um espaço cultural movimentado, enquanto as cachoeiras e a pedra atraem visitantes em busca de aventuras e vistas panorâmicas.
+                    </Phrase>
+                    <ButtonContainer device={windowSize}>
+                        <Button type="secondary" text="Reservar minha suíte" onClick={()=>console.log("Clicou")} size={windowSize === 'mobile'? 'xlarge':'large'} />
+                    </ButtonContainer>
+                </InfoContainer>
             </Information >
-            <img src={foto} alt="foto" style={{flex:1}}/>
+            {
+                windowSize !== 'mobile' && <Image />
+            }
         </Container>
     )
 }
